d50b46559ef91459c795598843596d602b355ffa
Aarav Shah <aarav.r.shah@gmail.com>
2026-04-19 13:48:09 -0500

    renamed

--- js/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-
-<script type="text/javascript">
-  // Supabase JS v2 is loaded by each page before this file
-  const SUPABASE_URL  = "https://zyjmmbhptbawauupedto.supabase.co";
-  const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5am1tYmhwdGJhd2F1dXBlZHRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMzkwMDYsImV4cCI6MjA3MTgxNTAwNn0.y9Z1LsuGc3CIwnXI7PWybdAiKwSmINOxQ7NwdFHwsRo";
-
-  // single shared client on window
-  window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
-</script>
